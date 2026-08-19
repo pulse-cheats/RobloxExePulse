fde8c95c4d02f2419753314129437e617ab5085c
pulse-cheats <gkalimanisgkalimanis@gmail.com>
2026-08-19 13:52:08 +0300

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define EXECUTOR_NAME "RobloxExecutor"
+#define EXECUTOR_VERSION "1.0.0"
+
+// UI Settings
+#define UI_WIDTH 400
+#define UI_HEIGHT 600
+
+// Logging
+#define LOG_ENABLED true
+
+#endif // CONFIG_H
